c3d0380333554fd77426126028db3dca84b5b9c9
Malika Umaralieva <malika0umaralieva@gmail.com>
2024-03-11 14:39:16 +0100

    Status_relationship

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    use HasFactory;
+    public function people(): HasMany
+    {
+        return $this->hasMany(Person::class);
+    }
 }
