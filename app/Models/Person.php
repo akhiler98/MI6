87ef2ee27898d09f2a65f3f4fb36081f16440891
Yusaiyin <yusaiyin89@gmail.com>
2024-03-11 14:49:42 +0100

    person relationships

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Alias;
+use App\Models\Image;
+use App\Models\Status;
 
 class Person extends Model
 {
     use HasFactory;
 
-    public function aliases() {
-        
+    public function alias() {
+        return $this->hasMany(Alias::class);
+    }
+
+    public function image() {
+        return $this->belongsTo(Image::class);
+    }
+
+        public function status() {
+        return $this->belongsTo(Status::class);
     }
 }
