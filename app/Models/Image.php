1834e19dc98906e36a1182c027f3d90a6673b248
akhiler <akhiler19@gmail.com>
2024-03-11 14:45:03 +0100

    make relationship in image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Person;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    public function person()
+    {
+        return $this->hasOne(Person::class);
+    }
 }
